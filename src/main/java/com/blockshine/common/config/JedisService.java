283f72f3de3d40ce0be73073cec49c591acb0e09
zhangliang <zhangliang@10.10.13.230>
2018-04-10 18:23:16 +0800

    redis 改造

--- src/main/java/com/blockshine/common/config/JedisService.java
+++ src/main/java/com/blockshine/common/config/JedisService.java
@@ -1,255 +1,258 @@
 package com.blockshine.common.config;
 
-import org.springframework.beans.BeansException;
-import org.springframework.context.ApplicationContext;
-import org.springframework.context.ApplicationContextAware;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
+
+import lombok.extern.slf4j.Slf4j;
 import redis.clients.jedis.Jedis;
 import redis.clients.jedis.JedisPool;
 
 import java.util.Set;
 
 @Component()
-public class JedisService implements ApplicationContextAware {
+@Slf4j(topic = "JedisService")
+public class JedisService {
 
-    ApplicationContext applicationContext = null;
+	@Autowired
+	private JedisPool jedisPool;
 
-    public static JedisPool jedisPool=  null;
-
-    public JedisService(){
-
-    }
-
-    public Jedis getJedis(){
-         Jedis jedis = null;
-        if (jedis ==null){
-            synchronized (Jedis.class){
-                if (jedis ==null){
-                    jedis = getJedisPool().getResource();
+	private Jedis jedis = null;  
+	 /** 
+     * 获取Jedis对象 
+     * @return 
+     */  
+    public synchronized  Jedis getJedis(){  
+        if(jedisPool != null){  
+            try{  
+                if(jedis == null ){  
+                     jedis = jedisPool.getResource();
                 }
-            }
-        }
-        return jedis;
-    }
-
-    public JedisPool getJedisPool(){
-
-        if (jedisPool ==null){
-            synchronized (JedisPool.class){
-                if (jedisPool==null){
-                    jedisPool = applicationContext.getBean(JedisPool.class);
-                }
-            }
-        }
-        return jedisPool;
-    }
-
-
-
-
-
-
-
-    @Override
-    public void setApplicationContext(ApplicationContext applicationContext) throws BeansException {
-        if(this.applicationContext == null){
-            this.applicationContext  = applicationContext; //初始化 spring applicationContext
-        }
-    }
-
-    /**
-     * 根据key查看是否存在
-     * @param key
-     * @return
-     */
-    public  boolean hasKey(String key){
-
-        Jedis jedis = getJedis();
-        try {
-            return jedis.exists(key);
-        }finally {
-            returnJedis(jedis);
-        }
+            }catch(Exception e){  
+//            	jedisPool.close();
+            	log.error("getJedis error"+e);
+            }  
+        }  
+        return jedis;  
+    }  
+      
+      
+    /** 
+     * 回收Jedis对象资源 
+     * @param jedis 
+     */  
+    public synchronized  void returnResource(Jedis  jedis){  
+        if(jedis != null){  
+            jedisPool.returnResource(jedis);  
+        }  
+    }  
+      
+    /** 
+     * Jedis对象出异常的时候，回收Jedis对象资源 
+     * @param jedis 
+     */  
+    public synchronized  void returnBrokenResource(Jedis  jedis){  
+        if(jedis != null){  
+            jedisPool.returnBrokenResource(jedis);  
+        }  
+    }  
 
 
-    }
+	/**
+	 * 根据key查看是否存在
+	 * 
+	 * @param key
+	 * @return
+	 */
+	public boolean hasKey(String key) {
 
-    /**
-     * 设置key -value 形式数据
-     * @param key
-     * @param value
-     * @return
-     */
-    public  String set(String key,String value){
-        Jedis jedis = getJedis();
-        try {
-            return  jedis.set(key,value);
-        }finally {
-            returnJedis(jedis);
-        }
+		Jedis jedis = getJedis();
+		try {
+			return jedis.exists(key);
+		}finally {
+			returnJedis(jedis);
+		}
 
-    }
+	}
 
-    /**
-     * 设置 一个过期时间
-     * @param key
-     * @param value
-     * @param timeOut 单位秒
-     * @return
-     */
-    public  String set(String key,String value,int timeOut){
-        Jedis jedis = getJedis();
-        try {
-            return jedis.setex(key,timeOut,value);
-        }finally {
-            returnJedis(jedis);
-        }
+	/**
+	 * 设置key -value 形式数据
+	 * 
+	 * @param key
+	 * @param value
+	 * @return
+	 */
+	public String set(String key, String value) {
+		Jedis jedis = getJedis();
+		try {
+			return jedis.set(key, value);
+		} finally {
+			returnJedis(jedis);
+		}
 
-    }
+	}
 
+	/**
+	 * 设置 一个过期时间
+	 * 
+	 * @param key
+	 * @param value
+	 * @param timeOut
+	 *            单位秒
+	 * @return
+	 */
+	public String set(String key, String value, int timeOut) {
+		Jedis jedis = getJedis();
+		try {
+			return jedis.setex(key, timeOut, value);
+		} finally {
+			returnJedis(jedis);
+		}
 
-    /**
-     * 对象hash
-     * 设置 一个过期时间
-     * @param key
-     * @param value
-     * @param timeOut 单位秒
-     * @return
-     */
-    public  String setex(String key,String value,int timeOut){
-        Jedis jedis = getJedis();
+	}
 
-        try {
-            return jedis.setex(key.getBytes(),timeOut,value.getBytes());
-        }finally {
-            returnJedis(jedis);
-        }
+	/**
+	 * 对象hash 设置 一个过期时间
+	 * 
+	 * @param key
+	 * @param value
+	 * @param timeOut
+	 *            单位秒
+	 * @return
+	 */
+	public String setex(String key, String value, int timeOut) {
+		Jedis jedis = getJedis();
 
-    }
+		try {
+			return jedis.setex(key.getBytes(), timeOut, value.getBytes());
+		} finally {
+			returnJedis(jedis);
+		}
 
+	}
 
-    /**
-     * 设置 一个过期时间
-     * @param key
-     * @return
-     */
-    public  byte[] getex(String key){
-        Jedis jedis = getJedis();
-        try {
-            return jedis.get(key.getBytes());
-        }finally {
-            returnJedis(jedis);
-        }
+	/**
+	 * 设置 一个过期时间
+	 * 
+	 * @param key
+	 * @return
+	 */
+	public byte[] getex(String key) {
+		Jedis jedis = getJedis();
+		try {
+			return jedis.get(key.getBytes());
+		} finally {
+			returnJedis(jedis);
+		}
 
+	}
 
-    }
+	/**
+	 * 根据key获取value
+	 * 
+	 * @param key
+	 * @return
+	 */
+	public String getByKey(String key) {
+		Jedis jedis = getJedis();
 
-    /**
-     * 根据key获取value
-     * @param key
-     * @return
-     */
-    public  String getByKey(String key){
-        Jedis jedis = getJedis();
+		try {
+			return jedis.get(key);
+		} finally {
+			returnJedis(jedis);
+		}
 
-        try {
-            return jedis.get(key);
-        }finally {
-            returnJedis(jedis);
-        }
+	}
 
-    }
+	/**
+	 * 根据通配符获取所有匹配的key
+	 * 
+	 * @param pattern
+	 * @return
+	 */
+	public Set<String> getKesByPattern(String pattern) {
+		Jedis jedis = getJedis();
 
-    /**
-     * 根据通配符获取所有匹配的key
-     * @param pattern
-     * @return
-     */
-    public  Set<String> getKesByPattern(String pattern){
-        Jedis jedis = getJedis();
+		try {
+			return jedis.keys(pattern);
+		} finally {
+			returnJedis(jedis);
+		}
 
-        try {
-            return jedis.keys(pattern);
-        }finally {
-            returnJedis(jedis);
-        }
+	}
 
-    }
+	/**
+	 * 根据key删除
+	 * 
+	 * @param key
+	 */
+	public void delByKey(String key) {
+		Jedis jedis = getJedis();
 
-    /**
-     * 根据key删除
-     * @param key
-     */
-    public  void delByKey(String key){
-        Jedis jedis = getJedis();
+		try {
+			jedis.del(key);
+		} finally {
+			returnJedis(jedis);
+		}
 
-        try {
-            jedis.del(key);
-        }finally {
-            returnJedis(jedis);
-        }
+	}
 
-    }
+	/**
+	 * 根据key获取过期时间
+	 * 
+	 * @param key
+	 * @return
+	 */
+	public long getTimeOutByKey(String key) {
+		Jedis jedis = getJedis();
 
-    /**
-     * 根据key获取过期时间
-     * @param key
-     * @return
-     */
-    public  long getTimeOutByKey(String key){
-        Jedis jedis = getJedis();
+		try {
+			return jedis.ttl(key);
+		} finally {
+			returnJedis(jedis);
+		}
 
-        try {
-            return jedis.ttl(key);
-        }finally {
-            returnJedis(jedis);
-        }
+	}
 
-    }
+	/**
+	 * 清空数据 【慎用啊！】
+	 */
+	public void flushDB() {
+		Jedis jedis = getJedis();
 
-    /**
-     * 清空数据 【慎用啊！】
-     */
-    public  void flushDB(){
-        Jedis jedis = getJedis();
+		try {
+			jedis.flushDB();
+		} finally {
+			returnJedis(jedis);
+		}
 
-        try {
-            jedis.flushDB();
-        }finally {
-            returnJedis(jedis);
-        }
+	}
 
+	/**
+	 * 刷新过期时间
+	 * 
+	 * @param key
+	 * @param timeOut
+	 * @return
+	 */
+	public long refreshLiveTime(String key, int timeOut) {
+		Jedis jedis = getJedis();
 
+		try {
+			return jedis.expire(key, timeOut);
+		} finally {
+			returnJedis(jedis);
+		}
 
-    }
+	}
 
-    /**
-     * 刷新过期时间
-     * @param key
-     * @param timeOut
-     * @return
-     */
-    public  long refreshLiveTime(String key,int timeOut){
-        Jedis jedis = getJedis();
-
-        try {
-            return jedis.expire(key,timeOut);
-        }finally {
-            returnJedis(jedis);
-        }
-
-
-    }
-
-
-
-    /**
-     * 回收jedis(放到finally中)
-     * @param jedis
-     */
-    public void returnJedis(Jedis jedis) {
-        if (null != jedis && null != jedisPool) {
-            jedisPool.returnResource(jedis);
-        }
-    }
+	/**
+	 * 回收jedis(放到finally中)
+	 * 
+	 * @param jedis
+	 */
+	public void returnJedis(Jedis jedis) {
+//		if (null != jedis && null != jedisPool) {
+//			jedisPool.returnResource(jedis);
+//		}
+	}
 }
